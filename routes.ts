adcc514597a199ec6514d74fd5711f3f9dfc6b42
Ajay-MAQ <konakallaa@maqsoftware.com>
2026-02-18 08:52:35 +0530

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -281,7 +281,7 @@ router.post("/create-stories", authMiddleware, async (req, res) => {
         { op: "add", path: "/fields/System.Description", value: story.description },
         { op: "add", path: "/fields/Microsoft.VSTS.Scheduling.StoryPoints", value: story.storyPoints },
         { op: "add", path: "/fields/Microsoft.VSTS.Common.Priority", value: story.priority },
-        { op: "add", path: "/fields/Microsoft.VSTS.Common.Risk", value: story.risk },
+        // { op: "add", path: "/fields/Microsoft.VSTS.Common.Risk", value: story.risk },
         {
           op: "add",
           path: "/relations/-",
